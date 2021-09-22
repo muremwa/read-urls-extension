109e1cb67ec614f97b632b178a86ed7376cdb210
Muremwa <danmburu254@gmail.com>
2021-09-22 20:48:05 +0300

    detection using admin module not models module

--- modelDetection.js
+++ modelDetection.js
@@ -1,5 +1,5 @@
 /* 
-    Detect automatically all models inside of a project
+    Detect automatically all models inside of a project from admin module
     using .detect
 */
 
@@ -17,6 +17,14 @@ const removeQuotationMarks = (text) => text.replace(/"/g, '').replace(/'/g, '');
 
 
 /**
+ * Remove whitespace from text
+ * @param {string} text 
+ * @returns {string} text without space
+ */
+const removeSpace = (text) => text.replace(/\s/g, '');
+
+
+/**
  * Remove comments from python code text
  * @param {string} pythonCodeText 
  * @returns {string} python code without comments
@@ -26,82 +34,133 @@ const removePythonComments = (pythonCodeText) => pythonCodeText.replace(/""".*?"
 
 /**
  * Get all model class names from text
+ * returns an object with the following keys:
+ *  1. registeredModels - models registered
+ *  2. inlineModels - models appearing as inline only i.e. they don't appear on changelist on admin site
+ * 
+ *  if a models is both inline and registered, it will appear on registeredModels only
+ * 
  * @param {string} fileText 
- * @returns {string[]} model class names
+ * @returns {{registeredModels: string[], inlineModels: string[]}} object with registered and inline models
  */
 function retrieveModelClassNames (fileText) {
     /* 
         Using text from a model file, retrieve all model classes
     */
-    const modelNames = []
+    
+    const registeredModels = [];
+    const inlineModels = [];
     fileText = removePythonComments(fileText);
 
-    // determine what a model is called (i.e. it has an alias like "from django.db.models import Model as alias")
-    const aliasMatch = fileText.match(/Model\sas\s(?<alias>\w+)/);
-    const modelPattern = aliasMatch && aliasMatch.groups && aliasMatch.groups.alias? new RegExp(`class.*?\\(${aliasMatch.groups.alias}\\):`, 'g'): /class.*?\(.*?Model\):/g;
+    // look for an alias for register? the search for registered models
+    const aliasMatch = fileText.match(/from django\.contrib\.admin\simport\sregister\sas\s(?<alias>.*?)$/m);
+    const registerName = aliasMatch && aliasMatch.groups.alias? aliasMatch.groups.alias: 'register';
+    const modelPattern = new RegExp(`${registerName}\\((?<model>.*?)[\\),]`, 'g')
 
-    // search fileText for classes inheriting from modelName
-    const rawModels = fileText.match(modelPattern);
-    
-    // extract the model name
-    if (rawModels) {
-        rawModels.forEach((model) => {
-            const posName = model.match(/class\s(?<model>.*?)\(/);
-            if (posName && posName.groups && posName.groups.model) {
-                modelNames.push(posName.groups.model);
-            };
-        });
+    let match = modelPattern.exec(fileText);
+
+    while (match) {
+        if (match.groups.model) {
+            registeredModels.push(removeSpace(match.groups.model));
+        };
+        match = modelPattern.exec(fileText);
     };
-    
-    return modelNames
+
+    // look for inline models
+    const inlineModelPattern = new RegExp(/model[\s]*=[\s]*(?<inlineModel>.*?)$/gm)
+    let inlineMatch = inlineModelPattern.exec(fileText);
+
+    while (inlineMatch) {
+        if (inlineMatch.groups.inlineModel && !registeredModels.includes(inlineMatch.groups.inlineModel)) {
+            inlineModels.push(removeSpace(inlineMatch.groups.inlineModel));
+        };
+        inlineMatch = inlineModelPattern.exec(fileText);
+    };
+
+    return { registeredModels, inlineModels }
 };
 
 
 /**
- * Takes the path of an app and gets text from files with models
+ * Takes the path of an app and gets text from the admin module
  * @param {string} appPath 
- * @returns {string[]} an array of files with models
+ * @returns {string[]} an array of text from the appPath admin module
  */
-function retrieveTextFromModelModules (appPath) {
+ function retrieveTextFromAdminModule (appPath) {
     /* 
-        Models in django live in either two places
-        1. projectroot/app/models.py
-        2. projectroot/app/models/*.py
+        Models in django are registered in the admin module
+        1. projectroot/app/admin.py
+        2. projectroot/app/admin/*
     */
     const files = [];
     const paths = [
-        path.join(appPath, 'models.py'),
-        path.join(appPath, 'models')
+        path.join(appPath, 'admin.py'),
+        path.join(appPath, 'admin')
     ];
 
     if (fs.existsSync(paths[0])) {
         files.push(fs.readFileSync(paths[0], {
             encoding: 'utf-8',
             flag: 'r'
-        }))
+        }));
     } else if (fs.existsSync(paths[1])) {
-        const initFile = path.join(paths[1], '__init__.py')
-        if (fs.existsSync(initFile)) {
-            const initCode = removePythonComments(fs.readFileSync(initFile, {
-                encoding: 'utf-8',
-                flag: 'r'
-            }))
-            const importRegex = new RegExp(/from\s(.*?)\simport/, 'g');
-            let match = importRegex.exec(initCode);
 
-            while (match) {
-                const file = match[1].includes('.')? match[1].replace(/\./g, '\\'): match[1];
-                files.push(fs.readFileSync(path.join(paths[1], `${file}.py`), {
+        const transformImportToPath = (importPath = '', imports = '') => {
+            /* 
+                receive imports and transform them to a path
+            */
+            const importPaths = [];
+
+            if (!importPath.startsWith('.')) {
+                importPath = importPath.replace(/.*?\.admin[\.]*/, '.');
+            };
+
+            const splitPath = importPath.split('.');
+            imports.split(',').map((import_) => importPaths.push(
+                [path.join(...splitPath, import_.trim()), import_.trim()]
+            ))
+            return importPaths;
+        };
+        
+        const getAllImports = (home) => {            
+            const initFile = path.join(home, '__init__.py');
+
+            if (fs.existsSync(initFile)) {
+                const initFileText = removePythonComments(fs.readFileSync(initFile, {
                     encoding: 'utf-8',
                     flag: 'r'
                 }));
-                match = importRegex.exec(initCode);
+                const importRegex = new RegExp(/from\s(?<path>.*?)\simport\s(?<import>.*?$)/mg);
+                let match = importRegex.exec(initFileText);
+
+                while (match) {
+                    if (match.groups.path && match.groups.import) {
+                        transformImportToPath(match.groups.path, match.groups.import).forEach((processedImport) => {
+                            const importAsFile = path.join(home, `${processedImport[0]}.py`);
+                            const theImport = processedImport[1];
+
+                            if (fs.existsSync(importAsFile)) {
+                                files.push(fs.readFileSync(importAsFile, {
+                                    encoding: 'utf-8',
+                                    flag: 'r'
+                                }));
+                            } else {
+                                getAllImports(path.join(home, theImport));
+                            };
+                        });
+                    };
+
+                    match = importRegex.exec(initFileText);
+                };
             };
         };
+
+        getAllImports(paths[1]);
     };
 
     return files;
-};
+ };
+
 
 
 /**
@@ -110,7 +169,7 @@ function retrieveTextFromModelModules (appPath) {
  * @param {boolean} registeredOnly only search registered apps only; default is false
  * @returns {{app: string[]}} object whose keys are apps and values are arrays of model class names
  */
-function searchModels (projectRootPath, registeredOnly = false) {
+ function searchModels (projectRootPath, registeredOnly = false) {
     const localApps = [];
 
     // project directories
@@ -173,8 +232,8 @@ function searchModels (projectRootPath, registeredOnly = false) {
     } else {
         localApps.push(...rootDirs.filter((dir) => {
             return [
-                path.join(projectRootPath, dir, 'models.py'),
-                path.join(projectRootPath, dir, 'models')
+                path.join(projectRootPath, dir, 'admin.py'),
+                path.join(projectRootPath, dir, 'admin')
             ].some((home) => fs.existsSync(home));
         }));
     };
@@ -182,8 +241,8 @@ function searchModels (projectRootPath, registeredOnly = false) {
     // loop through every app and retieve models
     const projectModels = {}
     localApps.forEach((app) => {
-        const modelClasses = retrieveTextFromModelModules(path.join(projectRootPath, app)).map((moduleText) => {
-            return retrieveModelClassNames(moduleText);
+        const modelClasses = retrieveTextFromAdminModule(path.join(projectRootPath, app)).map((moduleText) => {
+            return retrieveModelClassNames(moduleText).registeredModels;
         });
         
         if (modelClasses.length) {
